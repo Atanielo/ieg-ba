bf6a763f79df13c25360c265c5386eb2ce16fa90
root <root@debian>
2020-03-26 20:59:01 -0300

    segunda Configuring the app ieg

--- Comunidade/settings.py
+++ Comunidade/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost','192.168.100.5']
+ALLOWED_HOSTS = ['localhost','ieg-ba.herokuapp.com']
 
 
 # Application definition
